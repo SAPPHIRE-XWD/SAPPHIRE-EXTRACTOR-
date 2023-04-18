3f318549dc88e6f997f865ab68cd0d9efc9d8537
SAPPHIRE XWD <113887511+SAPPHIRE-XWD@users.noreply.github.com>
2023-04-18 22:47:52 +0545

    Add files via upload

---
+++ minatox.py
@@ -0,0 +1,12 @@
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://www.facebook.com/profile.php?id=100057149474543')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import minato
+elif bit == '32bit':
+    print('Sapphire is every where')
