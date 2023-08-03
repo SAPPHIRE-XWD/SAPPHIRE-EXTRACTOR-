6c937a41599fc8267ad3a75b3745f3a4bb443ea4
SAPPHIRE XWD <113887511+SAPPHIRE-XWD@users.noreply.github.com>
2023-08-03 19:51:31 +0545

    Update minatox.py

--- minatox.py
+++ minatox.py
@@ -7,6 +7,6 @@ os.system('xdg-open https://www.facebook.com/profile.php?id=100057149474543')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import minato
+    import Garryx
 elif bit == '32bit':
-    import minato
+    import Garryx
